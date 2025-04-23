556b3247b0205ac5b536aab0133bcea5df06e0ca
BOTCAHX <tioclkp02@gmail.com>
2025-04-24 00:39:29 +0700

    tes

---
+++ index.js
@@ -0,0 +1,10 @@
+const { ttdl, igdl, fbdown, twitter, youtube, aio } = require('./lib/module');
+
+module.exports = {
+  aio,
+  fbdown,
+  igdl,
+  ttdl,
+  twitter,
+  youtube,
+};
---
+++ test/test.js
@@ -0,0 +1,12 @@
+const { igdl } = require('../index');
+
+(async () => {
+    try {
+        console.log('Testing...');
+        let url = 'https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link'
+        const result = await igdl(url);
+        console.log('Response:', result);
+    } catch (error) {
+        console.error(error.message);
+    }
+})();
