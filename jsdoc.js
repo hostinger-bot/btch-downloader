5617057712439c57cfcdf26d7696d3b4977429a1
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-10-02 17:50:33 +0700

    release v 13

--- jsdoc.js
+++ jsdoc.js
@@ -2,7 +2,7 @@
  * @module btch-downloader
  * @description A lightweight TypeScript/JavaScript library for downloading media from social media platforms
  * @see {@link https://github.com/hostinger-bot/btch-downloader|GitHub Repository} for contributions and issue reporting.
- * @version 5.0.12
+ * @version 5.0.13
  * @author Tio
  * @license MIT
  */
